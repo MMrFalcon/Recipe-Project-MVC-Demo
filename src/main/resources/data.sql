d9f80dfa8aeba9511158d5ae3f0225f7760e1203
MMrFalcon <kubasokol123456@gmail.com>
2019-03-23 14:59:02 +0100

    Initialize Data via data.sql file. Closes #3

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO unit_of_measure (unit) VALUES ('spoons');
+INSERT INTO unit_of_measure (unit) VALUES ('glasses');
+INSERT INTO category (description) VALUES ('Fast-Food');
+INSERT INTO category (description) VALUES ('Oriental-Food');
