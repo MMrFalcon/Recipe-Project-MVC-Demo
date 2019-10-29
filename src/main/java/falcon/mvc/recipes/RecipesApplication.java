6be8886a7a9ded1606eab7e730eadbeabdd78968
MMrFalcon <kubasokol123456@gmail.com>
2019-10-29 18:56:45 +0100

    Prepare application for deploy to Tomcat server.#21

--- src/main/java/falcon/mvc/recipes/RecipesApplication.java
+++ src/main/java/falcon/mvc/recipes/RecipesApplication.java
@@ -2,12 +2,18 @@ package falcon.mvc.recipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RecipesApplication {
+public class RecipesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RecipesApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RecipesApplication.class);
+    }
 }
